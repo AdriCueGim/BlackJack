a51cc45c6cbe16f3f30af633e061aa5cd3aa6b77
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-04-21 09:56:47 +0200

    Dejo a mecias Mazo.cs

---
+++ BlackJack/Mazo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public class Mazo : IEnumerable<Carta>
+    {
+        private LinkedList<Carta> Cartas { get; set; }
+        public int NumeroCartas 
+        { 
+            get
+            { 
+                return Cartas.Count; 
+            } 
+        }
+        public Mazo()
+        {
+            Cartas = new LinkedList<Carta>();
+        }
+
+        public void AñadeBaraja()
+        {
+
+        }
+
+        public void AñadeCarta(Carta carta)
+        {
+            Cartas.AddLast(carta);
+        }
+
+        public Carta ExtraePrimera()
+        {
+            Carta primera = Cartas.First.Value;
+            Cartas.RemoveFirst();
+            return primera;
+        }
+
+        public IEnumerator<Carta> GetEnumerator()
+        {
+            return Cartas.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Cartas.GetEnumerator();
+        }
+    }
+}

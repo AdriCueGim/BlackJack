4a2c72f968cfe029992381693dbbdde56c1f77a5
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-04-28 09:56:26 +0200

    Añado la clase Apostador

---
+++ BlackJack/Apostador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public class Apostador : Jugador
+    {
+        public enum Accion 
+        { 
+            Plantarse, Doblarse, Carta 
+        }
+
+        public string Nombre { get; }
+        public int Saldo { get; private set; }
+        public override bool PuedePedirCarta
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public bool PuedeDoblarse { get; }
+        private ManoApostador _ManoApostador { get; }
+
+        public Apostador(string nombre, int saldo)
+        {
+            Nombre = nombre;
+            Saldo = saldo;
+        }
+
+        public void Plantarse()
+        {
+
+        }
+
+        public void Doblarse()
+        {
+
+        }
+
+        public void Empata()
+        {
+
+        }
+
+        public void Gana()
+        {
+
+        }
+
+        public void IniciaMano(int apuesta)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            
+        }
+    }
+}
--- BlackJack/Jugador.cs
+++ BlackJack/Jugador.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BlackJack
 {
-    public abstract class Jugador
+    public abstract class Jugador : IDisposable
     {
         public class Excepcion : Exception
         {
@@ -39,17 +39,29 @@ namespace BlackJack
 
         public void RecibeCarta(Carta c)
         {
-
+            if (!ManoIniciada)
+                throw new Excepcion("La mano no está iniciada, no se puede recibir carta.");
+            if (Mano.Cerrada)
+                throw new Excepcion("La mano está cerrada, no se puede recibir carta.");
+            Mano.AñadeCarta(c);
         }
 
         public IEnumerable<Carta> FinalizaMano()
         {
-            
+            if (!ManoIniciada)
+                throw new Excepcion("La mano no está iniciada, no se puede finalizar mano.");
+            if (Mano.Cerrada)
+                throw new Excepcion("La mano no está cerrada, no se puede finalizar.");
+            List<Carta> mano = new List<Carta>(Mano);
+            Mano = null;
+            return mano;
         }
 
         public void Dispose()
         {
-
+            if (ManoIniciada)
+                throw new Excepcion("No se puede destruir el objeto porque la mano está iniciada");
+            Mano = null;
         }
     }
 }

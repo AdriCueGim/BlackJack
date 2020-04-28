09b7cd3baf232606a829e5ba62bfda472bbda8b3
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-04-28 08:01:52 +0200

    Empiezo Jugador.cs

---
+++ BlackJack/Jugador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public abstract class Jugador
+    {
+        public class Excepcion : Exception
+        {
+            public Excepcion(string message) : base(message)
+            {
+            }
+            public Excepcion(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+        }
+
+        public ManoBlackJack Mano { get; protected set; }
+        public abstract bool PuedePedirCarta { get; }
+        public bool ManoIniciada 
+        { 
+            get
+            {
+                return Mano != null;
+            }
+        }
+        public bool PuedeRecibirCarta 
+        { 
+            get
+            {
+                return ManoIniciada && !Mano.Cerrada;
+            }
+        }
+
+        protected Jugador()
+        {
+        }
+
+        public void RecibeCarta(Carta c)
+        {
+
+        }
+
+        public IEnumerable<Carta> FinalizaMano()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}

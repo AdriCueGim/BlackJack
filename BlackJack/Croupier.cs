b236ff2da711add09bd7a7d802898b4ecd115f36
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-04-29 12:06:16 +0200

    Subo clase Croupier

---
+++ BlackJack/Croupier.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public class Croupier : Jugador
+    {
+        private Mazo MazoReparto { get; }
+        private Mazo MazoRetirada { get; }
+        private int NumeroBarajas { get; }
+        public int CartasEnReparto { get; }
+        public int CartasRetiradas { get; }
+        public override bool PuedePedirCarta
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public bool MazosInicializados { get; }
+
+        public Croupier()
+        {
+
+        }
+
+        public void MezclaMazoReparto()
+        {
+
+        }
+
+        public void AñadeBarajasAlMazoReparto()
+        {
+
+        }
+
+        public void ReiniciaMazoRepartoConCartasRetiradas()
+        {
+
+        }
+
+        public void ReparteCarta(Jugador j)
+        {
+
+        }
+
+        public void RetiraMano(Jugador j)
+        {
+
+        }
+
+        public void GestionaFichasAlFinalizarMano(Apostador a)
+        {
+
+        }
+        public void IniciaMano()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Croupier";
+        }
+    }
+}
